5b7b257d96d23a8ed520fe75b7b2f49af46d894a
amos <fabiaamosc@gmail.com>
2023-03-16 13:20:18 +0800

    removed msgcount & msgid

--- Send_Lora.ino
+++ Send_Lora.ino
@@ -27,7 +27,6 @@ void sendMessage(String outgoing) {
   LoRa.beginPacket();                   // start packet
   LoRa.write(destination);              // add destination address
   LoRa.write(localAddress);             // add sender address
-  LoRa.write(msgCount);                 // add message ID
   LoRa.write(outgoing.length());        // add payload length
   LoRa.print(outgoing);                 // add payload
   LoRa.endPacket();                     // finish packet and send it                          // increment message ID
@@ -49,7 +48,6 @@ void receiveFromLoRa(){
   // read packet header bytes:
   int recipient = LoRa.read();          // recipient address
   byte sender = LoRa.read();            // sender address
-  byte incomingMsgId = LoRa.read();     // incoming msg ID
   byte incomingLength = LoRa.read();    // incoming msg length
 
   String incoming = "";
--- fishcounter.ino
+++ fishcounter.ino
@@ -17,14 +17,14 @@ int firstpress = 0; //for reset button
 void setup() {
   
   pinMode(pin_1, INPUT);
-  pinMode(pin_2, INPUT);
-  pinMode(pin_3, INPUT);
+//  pinMode(pin_2, INPUT);
+//  pinMode(pin_3, INPUT);
   pinMode(rstbtn, INPUT_PULLUP);
   Serial.begin(9600);
 
   oldstate = digitalRead(pin_1);
-  oldstate_2 = digitalRead(pin_2);
-  oldstate_3 = digitalRead(pin_3);
+//  oldstate_2 = digitalRead(pin_2);
+//  oldstate_3 = digitalRead(pin_3);
 
   LoRaSetup();
 }
@@ -32,8 +32,8 @@ void setup() {
 void loop() {
 
   oldstate = checkState(pin_1, oldstate);
-  oldstate_2 = checkState(pin_2, oldstate_2);
-  oldstate_3 = checkState(pin_3, oldstate_3);
+//  oldstate_2 = checkState(pin_2, oldstate_2);
+//  oldstate_3 = checkState(pin_3, oldstate_3);
 
 
 }
